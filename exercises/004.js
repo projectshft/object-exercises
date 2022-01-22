73af8cc5e39353ab6b5572b343fd1cf3ebc796cb
HK Nelson <94695428+hkn8@users.noreply.github.com>
2022-01-21 23:21:04 -0500

    fixes spacing and object method to not use

--- exercises/004.js
+++ exercises/004.js
@@ -11,6 +11,7 @@ Example input:
 Function's return value (output):
 ['Krysten', 33, false]
 
+Do not use "Object.values" to solve this prompt.
 Note that the input may have a different number of keys and values than the given sample. 
 
 E.g. it should also handle an input like:
@@ -28,11 +29,6 @@ Starter Code
 */
 
 function listAllValues(obj) {
-  var returnArr = [];
-
-  for (var key in obj) {
-     returnArr.push(obj[key]);
-  }
-
-  return returnArr;
+  //your code here
+  
 }
