2c9b28bcb34774e536d7c2a3cc97eb7ff7a9a7a4
HK Nelson <94695428+hkn8@users.noreply.github.com>
2022-01-21 23:21:42 -0500

    fixes spacing and object method to not use

--- exercises/006.js
+++ exercises/006.js
@@ -1,14 +1,17 @@
 /*
 Write  a function called "convertObjectToList" which converts an object literal into an array of arrays, like this:
+
 Argument:
 {
   name: 'Holly',
   age: 35,
   role: 'producer'
 }
+
 Return value:
 [['name', 'Holly'], ['age', 35], ['role', 'producer']]
 
+Do not use "Object.entries" to solve this prompt.
 Note that your function should be able to handle ANY object like this, not just the exact sample provided above.
 
 E.g., it should also be able to handle this, or any other object containing simple key-value pairs.
@@ -22,14 +25,6 @@ Starter Code:
 */
 
 function convertObjectToList(obj) {
-  const returnArr = [];
-
-  for (var key in obj) {
-    const returnNestedArr = [];
-    
-    returnNestedArr.push(key, obj[key]);
-    returnArr.push(returnNestedArr);
-  }
-
-  return returnArr;
+  //your code here
+  
 }
